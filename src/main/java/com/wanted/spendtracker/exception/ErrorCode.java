f04c205eeb1ca18348a5411a3924d3035f843e5a
sohyuneeee <sohyuney@naver.com>
2023-11-11 15:12:27 +0900

    feat #5: 사용자 회원가입 시 계정명에 대한 제한 사항 추가 및 validation message 추가

--- src/main/java/com/wanted/spendtracker/exception/ErrorCode.java
+++ src/main/java/com/wanted/spendtracker/exception/ErrorCode.java
@@ -17,7 +17,11 @@ public enum ErrorCode {
     MEMBER_PASSWORD_REPEATED("3회 이상 연속되는 문자를 포함한 비밀번호는 사용할 수 없습니다.", BAD_REQUEST),
     MEMBER_PASSWORD_SIMPLE("비밀번호는 숫자, 영문, 특수문자 중 2가지 이상을 포함해야 합니다.", BAD_REQUEST),
     MEMBER_PASSWORD_PERSONAL_INFO("다른 개인정보와 유사한 비밀번호는 사용할 수 없습니다.", BAD_REQUEST),
-    MEMBER_ACCOUNT_NAME_DUPLICATED("이미 같은 이름의 계정이 존재합니다", BAD_REQUEST)
+    MEMBER_PASSWORD_BLANK("비밀번호는 공백일 수 없습니다.", BAD_REQUEST),
+    MEMBER_ACCOUNT_NAME_DUPLICATED("이미 같은 이름의 계정이 존재합니다", BAD_REQUEST),
+    MEMBER_ACCOUNT_NAME_SHORT("계정 이름은 최소 2자 이상이어야 합니다", BAD_REQUEST),
+    MEMBER_ACCOUNT_NAME_LONG("계정 이름은 최대 20자까지 작성할 수 있습니다.", BAD_REQUEST),
+    MEMBER_ACCOUNT_NAME_BLANK("계정 이름은 공백일 수 없습니다.", BAD_REQUEST),
 
     ;
 
