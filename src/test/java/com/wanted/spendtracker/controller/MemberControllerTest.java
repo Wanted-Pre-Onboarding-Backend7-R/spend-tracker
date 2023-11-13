aad4fb103434f24c9a44ac46b5557096727de622
sohyuneeee <sohyuney@naver.com>
2023-11-13 16:13:55 +0900

    test #5: controller 통합테스트 수정 및 단위테스트 추가

--- src/test/java/com/wanted/spendtracker/controller/MemberControllerTest.java
+++ src/test/java/com/wanted/spendtracker/controller/MemberControllerTest.java
@@ -1,17 +1,19 @@
 package com.wanted.spendtracker.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wanted.spendtracker.domain.Member;
 import com.wanted.spendtracker.dto.request.MemberSignUpRequest;
 import com.wanted.spendtracker.exception.ErrorCode;
+import com.wanted.spendtracker.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.wanted.spendtracker.TestConstants.ACCOUNT_NAME;
 import static com.wanted.spendtracker.TestConstants.PASSWORD;
@@ -22,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("/api/members 통합 테스트")
 @AutoConfigureMockMvc
 @SpringBootTest
+@Transactional
 class MemberControllerTest {
 
     private static final String URI = "/api/members";
@@ -32,40 +35,49 @@ class MemberControllerTest {
     @Autowired
     private ObjectMapper mapper;
 
+    @Autowired
+    private MemberRepository memberRepository;
 
-    @DisplayName("사용자 회원가입")
-    @Nested
-    class Signup {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-        @DisplayName("성공")
-        @Test
-        void givenValidInfo_then201() throws Exception {
-            doJoin().andExpect(status().isCreated())
-                    .andExpect(jsonPath("$").doesNotExist())
-                    .andDo(print())
-                    .andReturn();
-        }
+    @DisplayName("성공")
+    @Test
+    void signUpMember() throws Exception {
+        MemberSignUpRequest memberSignUpRequest = MemberSignUpRequest.of(ACCOUNT_NAME, PASSWORD);
+        String content = mapper.writeValueAsString(memberSignUpRequest);
+        mockMvc.perform(post(URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").doesNotExist())
+                .andDo(print())
+                .andReturn();
+    }
 
-        @DisplayName("계정 이름 중복")
-        @Test
-        void givenDuplicateAccountName_then400() throws Exception {
-            ErrorCode errorCode = ErrorCode.MEMBER_ACCOUNT_NAME_DUPLICATED;
-            doJoin().andExpect(status().isBadRequest())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$.errorCode").value(errorCode.name()))
-                    .andExpect(jsonPath("$.message").value(errorCode.getMessage()))
-                    .andDo(print())
-                    .andReturn();
-        }
+    @DisplayName("계정 이름 중복")
+    @Test
+    void signUpMember_accountNameDuplicated() throws Exception {
+        //given
+        Member member = Member.builder()
+                .accountName(ACCOUNT_NAME)
+                .password(passwordEncoder.encode(PASSWORD)).build();
+        memberRepository.save(member);
 
-        private ResultActions doJoin() throws Exception {
-            MemberSignUpRequest memberSignUpRequest = MemberSignUpRequest.of(ACCOUNT_NAME, PASSWORD);
-            String content = mapper.writeValueAsString(memberSignUpRequest);
-            return mockMvc.perform(post(URI)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(content));
-        }
+        ErrorCode errorCode = ErrorCode.MEMBER_ACCOUNT_NAME_DUPLICATED;
+        MemberSignUpRequest memberSignUpRequest = MemberSignUpRequest.of(ACCOUNT_NAME, PASSWORD);
+        String content = mapper.writeValueAsString(memberSignUpRequest);
 
+        //when, then
+        mockMvc.perform(post(URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.errorCode").value(errorCode.name()))
+                .andExpect(jsonPath("$.message").value(errorCode.getMessage()))
+                .andDo(print())
+                .andReturn();
     }
 
 }
