9bfe513b7a1446161e27324aa47cfbd00ccb8688
sohyuneeee <sohyuney@naver.com>
2023-11-15 19:14:22 +0900

    feat #8: 카테고리 목록 조회 API 구현

--- src/main/java/com/wanted/spendtracker/service/CategoriesService.java
+++
@@ -1,9 +0,0 @@
-package com.wanted.spendtracker.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class CategoriesService {
-}
